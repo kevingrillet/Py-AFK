0dcf0c4de555698e314eb0e02b9fe2feea4bb073
kevingrillet <kevin.grillet@gmail.com>
2021-06-18 07:15:33 +0200

    Rename / Refacto

--- handler_adb.py
+++ handler_adb.py
@@ -9,7 +9,34 @@ import numpy as np
 import utils
 
 
-def adb_execute(command):
+def check():
+    """
+        Check if adb repository exists, if not try to download it
+    """
+    print("check")
+    if not os.path.exists("adb/"):
+        print("adb not found")
+        if platform.system() == "Windows":
+            install("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
+        elif platform.system() == "Darwin":
+            install("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
+        elif platform.system() == "Linux":
+            install("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
+    else:
+        print("adb found")
+
+
+def connect(device=""):
+    """
+        Check if device is connected
+    """
+    print("connect [device=" + device + "]")
+    if device == "nox":
+        return execute("connect localhost:62001")
+    return execute("get-state")
+
+
+def execute(command):
     """
         execute command, return stdout & stderr
     """
@@ -18,50 +45,23 @@ def adb_execute(command):
     return pipe.stdout.read()
 
 
-def check_adb():
-    """
-        Check if adb repository exists, if not try to download it
-    """
-    print("check_adb")
-    if not os.path.exists("adb/"):
-        print("adb not found")
-        if platform.system() == "Windows":
-            install_adb("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
-        elif platform.system() == "Darwin":
-            install_adb("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
-        elif platform.system() == "Linux":
-            install_adb("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
-    else:
-        print("adb found")
-
-
-def install_adb(url):
+def install(url):
     """
         Download ADB and unzip it in the current repository then delete the .zip
     """
-    print("install_adb [url=" + url + "]")
+    print("install [url=" + url + "]")
     filename = utils.download_file(url)
     utils.unzip(filename, "adb/")
     os.remove(filename)
 
 
-def check_device(device=""):
-    """
-        Check if device is connected
-    """
-    print("check_device [device=" + device + "]")
-    if device == "nox":
-        return adb_execute("connect localhost:62001")
-    return adb_execute("get-state")
-
-
-def restart_adb():
+def restart():
     """
         Restart ADB
     """
-    print("restart_adb")
-    adb_execute("kill-server")
-    adb_execute("start-server")
+    print("restart")
+    execute("kill-server")
+    execute("start-server")
 
 
 def screenshot():
@@ -69,7 +69,7 @@ def screenshot():
         Take a screenshot and return it
     """
     print("screenshot")
-    image_bytes = adb_execute("shell screencap -p").replace(b'\r\n', b'\n')
+    image_bytes = execute("shell screencap -p").replace(b'\r\n', b'\n')
     return cv2.imdecode(np.fromstring(image_bytes, np.uint8), cv2.IMREAD_COLOR)
 
 
@@ -79,8 +79,8 @@ def start_app(app="", sleep_timer=0):
     """
     print("start_app [app=" + app + ", sleep_timer=" + str(sleep_timer) + "]")
     if app:
-        # adb_execute("shell am start -n " + app)
-        adb_execute("shell monkey -p " + app + " 1")
+        # execute("shell am start -n " + app)
+        execute("shell monkey -p " + app + " 1")
         time.sleep(sleep_timer)
 
 
@@ -90,4 +90,4 @@ def stop_app(app=""):
     """
     print("stop_app [app=" + app + "]")
     if app:
-        adb_execute("shell am force-stop " + app)
+        execute("shell am force-stop " + app)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2 as cv
 
-import handler_adb
+import handler_adb as adb
 
-handler_adb.check_adb()
-handler_adb.restart_adb()
-handler_adb.check_device()
-handler_adb.start_app("com.lilithgame.hgame.gp", 30)
+adb.check()
+adb.restart()
+adb.connect()
+adb.stop_app("com.lilithgame.hgame.gp")
+adb.start_app("com.lilithgame.hgame.gp", 30)
 
-image = handler_adb.screenshot()
+image = adb.screenshot()
 cv.imshow("", image)
 cv.waitKey(0)
 cv.destroyWindow("")
