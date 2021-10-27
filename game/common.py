32a84e9371276b9002b57ec47235e8c6fff02fb2
kevingrillet <kevin.grillet@gmail.com>
2021-10-27 17:57:04 +0200

    WIP common

--- game/common.py
+++ game/common.py
@@ -1,53 +1,137 @@
+import time
+from enum import auto
+
+from utils.cls.SuperIntEnum import SuperIntEnum
 from utils.handler_cv2 import HandlerCv2
+
+
+class StartingStep(SuperIntEnum):
+    INIT = auto()
+    LOADING = auto()
+    UNDER_MAINTENANCE = auto()
+    POPUP = auto()
+    UPDATING = auto()
+    STARTED = auto()
+    DONE = auto()
 
 
 class Common:
     def __init__(self, cv2: HandlerCv2):
         self.cv2 = cv2
-        self.images_list = ["images/common/campaign.jpg", "images/common/update.jpg"]
+        self.images_list = ["./images/common/board_tales.jpg", "./images/common/campaign.jpg",
+                            "./images/common/cross.jpg", "./images/common/load.jpg",
+                            "./images/common/maintenance.jpg", "./images/common/special_gift.jpg",
+                            "./images/common/update.jpg"]
         self.images = self.cv2.load_images(self.images_list)
-        self.state = None
+        self.step = StartingStep.INIT
 
-    def close_main_menu_popup(self):
-        self.state = "game_is_starting"
-        while self.state == "game_is_starting":
-            data_image = self.images["images/common/load.jpg"]
+    def check_game_to_load(self) -> bool:
+        """
+            Check if afk arena title is visible (during start)
+        """
+        data_image = self.images["./images/common/load.jpg"]
+        if self.cv2.check_match(data_image):
+            self.step = StartingStep.LOADING
+            return True
+        elif self.step == StartingStep.LOADING:
+            self.step = self.step.next()
+        return False
+
+    def check_maintenance(self) -> bool:
+        """
+            Check if maintenance popup is visible
+            ToDo:
+                - Add image
+                - Restart the app after X secs
+        """
+        data_image = self.images["./images/common/maintenance.jpg"]
+        if self.cv2.check_match(data_image):
+            self.step = StartingStep.UNDER_MAINTENANCE
+            return True
+        elif self.step == StartingStep.UNDER_MAINTENANCE:
+            self.step = self.step.first()
+        return False
+
+    def check_popup(self) -> bool:
+        """
+            Check if there is any visible popup (after game menu is loaded)
+                - New hero
+                - Special gift
+                - Boarf Tales
+        """
+        data_image = self.images["./images/common/cross.jpg"]
+        if self.cv2.check_match(data_image):
+            self.step = StartingStep.POPUP
+            self.cv2.tap_find(1)
+            return True
+        else:
+            data_image = self.images["./images/common/special_gift.jpg"]
             if self.cv2.check_match(data_image):
-                self.cv2.tap_find(1)
+                self.step = StartingStep.POPUP
+                self.cv2.adb.tap(10, 10, 1)
+                return True
             else:
-                data_image = self.images["images/common/load.jpg"]
+                data_image = self.images["./images/common/board_tales.jpg"]
                 if self.cv2.check_match(data_image):
-                    self.cv2.tap_find(1)
-                else:
-                    self.state = 'game_ready'
+                    self.step = StartingStep.POPUP
+                    self.cv2.adb.tap(10, 10, 1)
+                    return True
+                elif self.step == StartingStep.POPUP:
+                    self.step = self.step.next()
+        return False
+
+    def check_started(self) -> bool:
+        """
+            Check if the Campaign buton is visible 3 times with 1 sec of sleep between each
+            If it fails, return false & set step to first
+        """
+        data_image = self.images["./images/common/campaign.jpg"]
+        for i in range(3):
+            self.cv2.adb.require_new_capture = True
+            if not self.cv2.check_match(data_image):
+                self.step = self.step.first()
+                return False
+            time.sleep(1)
+        self.step = StartingStep.DONE
+        return True
+
+    def check_update(self) -> bool:
+        """
+            Check if update logo is visible in game
+        """
+        data_image = self.images["./images/common/update.jpg"]
+        if self.cv2.check_match(data_image):
+            self.step = StartingStep.UPDATING
+            self.enable_fast_update()
+            return True
+        elif self.step == StartingStep.UPDATING:
+            self.step = self.step.next()
+        return False
+
+    def enable_fast_update(self):
+        """
+            If game is updating, try to enable fast update then close the window
+        """
+        self.cv2.tap_find(1)
+        data_image = self.images["./images/common/update.jpg"]
+        if self.cv2.check_match(data_image):
+            self.cv2.tap_find(1)
+        self.cv2.adb.tap(10, 10, 1)
 
     def run(self):
-        self.state = "Init"
-        self.wait_game_to_load()
-        self.wait_maintenance()
-        self.wait_update()
-        self.close_main_menu_popup()
-
-    def wait_game_to_load(self):
-        data_image = self.images["images/common/load.jpg"]
-        if not self.cv2.check_match(data_image):
-            self.state = "game_is_loading"
-        while self.state == "game_is_loading":
-            if not self.cv2.check_match(data_image):
-                self.state = "game_loaded"
-
-    def wait_maintenance(self):
-        data_image = self.images["images/common/maintenance.jpg"]
-        if self.cv2.check_match(data_image):
-            self.state = "game_is_under_maintenance"
-        while self.state == "game_is_under_maintenance":
-            if not self.cv2.check_match(data_image):
-                self.state = "game_is_updating"
-
-    def wait_update(self):
-        data_image = self.images["images/common/update.jpg"]
-        if self.cv2.check_match(data_image):
-            self.state = "game_is_updating"
-        while self.state == "game_is_updating":
-            if not self.cv2.check_match(data_image):
-                self.state = "game_updated"
+        """
+            Need to be launch after adb command to start the game
+            It will try to do everything until game is loaded and in main menu
+        """
+        self.step = StartingStep.INIT
+        while self.step != StartingStep.DONE:
+            if self.step <= StartingStep.LOADING and self.check_game_to_load():
+                continue
+            # if self.step <= StartingStep.UNDER_MAINTENANCE and self.check_maintenance():
+            #     continue
+            if self.step <= StartingStep.POPUP and self.check_popup():
+                continue
+            if self.step <= StartingStep.UPDATING and self.check_update():
+                continue
+            if self.step <= StartingStep.STARTED and self.check_started():
+                continue
--- main.py
+++ main.py
@@ -1,41 +1,38 @@
-from game import after, before, campaign, common as cu, dark_forest, heroes, ranhorn
+from game import campaign, common as cu, dark_forest, ending, heroes, ranhorn
 from utils import constant, handler_adb, handler_config, handler_cv2
 
 # Init everything
-_cfg = handler_config.HandlerConfig("config/config.ini")
-_cfg.init()
+# _cfg = handler_config.HandlerConfig("config/config.ini")
+# _cfg.init()
 
 _adb = handler_adb.HandlerAdb()
 _adb.init()
-_adb.start(constant.PACKAGE_NAME, 0)
+# _adb.start(constant.PACKAGE_NAME, 0)
 
 _cv2 = handler_cv2.HandlerCv2(_adb)
-_cv2.dev()
+# _cv2.dev()
+_cv2.show_debug_image = True
 
 # First things first
 _game_common = cu.Common(_cv2)
-# _game_common.run()
-
-# Before
-_game_before = before.Before(_cv2)
-# _game_before.run()
+_game_common.run()
 
 # Campaign tab
 _game_campaign = campaign.Campaign(_cv2)
 # _game_campaign.run()
 
 # Dark Forest
-_game_dark_forest = dark_forest.DarkForest(_cv2)
+# _game_dark_forest = dark_forest.DarkForest(_cv2)
 # _game_dark_forest.run()
 
 # Ranhorn
-_game_ranhorn = ranhorn.Ranhorn(_cv2)
+# _game_ranhorn = ranhorn.Ranhorn(_cv2)
 # _game_ranhorn.run()
 
 # Heroes
-_game_heroes = heroes.Heroes(_cv2)
+# _game_heroes = heroes.Heroes(_cv2)
 # _game_heroes.run()
 
 # End
-_game_after = after.After(_cv2)
+# _game_ending = ending.Ending(_cv2)
 # _game_after.run()
